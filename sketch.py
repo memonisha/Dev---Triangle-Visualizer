dafc301cd1a10c756154e7feb2cba64ad1e8b87f
memonisha <75033521+memonisha@users.noreply.github.com>
2024-03-09 10:07:35 +0530

    Add files via upload

--- sketch.py
+++ sketch.py
@@ -34,6 +34,7 @@ def draw():
   cordtext ()
   distances ()
   angles ()
+  
 
 def cordtext ():
   fill ("white")
@@ -61,17 +62,54 @@ def distances ():
   fill (9, 235, 99)
   text (f"{distance3}", 300, 30)
 
+  fill (255, 255, 255)
+  if distance1==distance2 and distance2==distance3:
+    text ("This triangle is equilateral", 800, 90)
+  elif distance1==distance2 or distance2==distance3 or distance3==distance1:
+    text ("This triangle is isosceles", 800, 90)
+  else:
+    text ("This triangle is scalene", 800, 90)
+
+
+  
+  
+
+
+
 def angles ():
   global angle1, angle2, angle3 
   #a = distance1
   #b = distance2
   #c = distance3
-  Angle1 = acos ((distance2**2+distance3**2-distance1**2)/2*distance2*distance3)
-  Angle2 = acos ((distance1**2+distance3**2-distance2**2)/2*distance1*distance3)
-  Angle3 = acos ((distance1**2+distance2**2-distance3**2)/2*distance1*distance2)
+  '''
+  Angle1 = acos((distance2**2+distance3**2-distance1**2)/2*distance2*distance3)
+  Angle2 = acos((distance1**2+distance3**2-distance2**2)/2*distance1*distance3)
+  Angle3 = acos((distance1**2+distance2**2-distance3**2)/2*distance1*distance2)
   angle1 = round(Angle1)
   angle2 = round(Angle2)
   angle3 = round(Angle3)
+  '''
+
+  
+  a = distance1
+  b = distance2
+  c = distance3
+  a2=a*a
+  b2=b*b
+  c2=c*c
+  angle1 = round(acos((b2+c2-a2) / (2*b*c)))
+  angle2 = round(acos((a2+c2-b2) / (2*a*c)))
+  angle3 = round(180 - (angle1 + angle2))
+  fill (255, 255, 255)
+  if angle1==90 or angle2==90 or angle3==90:
+    text ("This angle is right-angled", 800, 70)
+  elif angle1<90 and angle2<90 and angle3<90:
+    text ("This angle is acute", 800, 70)
+  elif angle1 > 90 or angle2 > 90 or angle3 > 90:
+    text ("This angle is obtuse", 800, 70)
+  
+  
+  
   fill ("White")
   text ("-ANGLES-", 600, 90)
   text (f"{angle1}", 600, 70)
